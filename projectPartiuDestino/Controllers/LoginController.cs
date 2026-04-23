3a3dc79b43e374034ee60c2fb8370af9db209c15
NicolasMessias080 <NickZzz2019@gmail.com>
2026-04-23 17:47:44 -0300

    feat: add MySQL dependency and database connection

--- projectPartiuDestino/Controllers/LoginController.cs
+++ projectPartiuDestino/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using projectPartiuDestino.Data;
 
 namespace projectPartiuDestino.Controllers
 {
@@ -6,13 +8,15 @@ namespace projectPartiuDestino.Controllers
     {
         public IActionResult Index()
         {
+            Conexao conexao = new Conexao();
+
+            using (MySqlConnection conn = conexao.ObterConexao())
+            {
+                conn.Open();
+                Console.WriteLine("Conectado com sucesso!");
+            }
+
             return View();
-        }
-        
-        // cad
-        public IActionResult Cadastro()
-        {
-            return RedirectToAction("Index", "Cadastro");
         }
     }
 }
---
+++ projectPartiuDestino/Data/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace projectPartiuDestino.Data
+{
+    public class Conexao
+    {
+        //altera aqui pra sua senha e o seu root, o meu ta diferente do padrão
+        private string dadosConexao =
+            "Server=localhost;Database=bdpartiudestino;Uid=root;Pwd=nicolas123;";
+
+        public MySqlConnection ObterConexao()
+        {
+            return new MySqlConnection(dadosConexao);
+        }
+    }
+}
