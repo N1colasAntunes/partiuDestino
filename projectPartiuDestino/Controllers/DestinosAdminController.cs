4524b07b4749c2e24b73ccd200de5b534e82975d
Marcos André <mc506526@gmail.com>
2026-05-19 15:12:29 -0300

    feat: adding button exclude from admin

--- projectPartiuDestino/Controllers/DestinosAdminController.cs
+++ projectPartiuDestino/Controllers/DestinosAdminController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
 
 namespace projectPartiuDestino.Controllers
 {
     public class DestinosAdminController : Controller
     {
+        private string conexao = "server=localhost;database=bdpartiudestino;uid=root;pwd=12345678;";
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        // =========================
+        // EXCLUIR DESTINO
+        // =========================
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                conn.Open();
+
+                string sql = "DELETE FROM destinos WHERE id = @id";
+
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
--- projectPartiuDestino/Controllers/PacotesAdminController.cs
+++ projectPartiuDestino/Controllers/PacotesAdminController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
 
 namespace projectPartiuDestino.Controllers
 {
     public class PacotesAdminController : Controller
     {
+        private string conexao = "server=localhost;database=bdpartiudestino;uid=root;pwd=12345678;";
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                conn.Open();
+
+                string sql = "DELETE FROM pacotes WHERE id = @id";
+
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
