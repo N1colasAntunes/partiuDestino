c85b6f3075540c835b80c63f0bd27adb2088b8bd
Marcos <mc506526@gmail.com>
2026-06-05 19:29:47 -0300

    feat(legal): add controller and views for terms of use and privacy policy

---
+++ projectPartiuDestino/Controllers/Legalcontroller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projectPartiuDestino.Controllers
+{
+
+    public class LegalController : Controller
+    {
+
+        public IActionResult TermosDeUso()
+        {
+            return View();
+        }
+
+        public IActionResult PoliticaDePrivacidade()
+        {
+            return View();
+        }
+    }
+}
